312853b41cd568a9420b70d74d3a309b76c05bc9
Nick Watts <nick@nawatts.com>
2016-11-22 20:03:10 -0500

    Install zplug in setup script

--- setup.zsh
+++ setup.zsh
@@ -15,6 +15,11 @@ if [[ $REPLY != "y" ]]; then
   exit 1
 fi
 
+# Install zplug
+# https://github.com/zplug/zplug
+export ZPLUG_HOME=$HOME/.zplug
+git clone "https://github.com/zplug/zplug" "${ZPLUG_HOME}"
+
 # Link dotfiles to home directory
 for source in $(find -H $DOTFILES -name '*.link'); do
   dest="$HOME/.${source:t:r}"
