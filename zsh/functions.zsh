b4907c4b4fd6a485b9d09859480abbf3ba9bb2c5
Nick Watts <nick@nawatts.com>
2017-01-11 22:33:37 -0500

    Add repeat-until-fails function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,1 +1,13 @@
 alias externalip="echo -n $(curl --silent 'http://checkip.amazonaws.com')"
+
+# Run a command repeatedly until it fails
+function repeat-until-fails () {
+  exit_code=0
+  count=0
+  while [ $exit_code -eq 0 ]; do
+    count=$((count + 1))
+    $@
+    exit_code=$?
+  done
+  echo "Ran ${count} times"
+}
