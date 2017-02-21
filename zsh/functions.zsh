be9a9132b469bdf22e141a917dd7f67a5704098c
Nick Watts <nick@nawatts.com>
2017-02-20 22:14:31 -0500

    Require argument for repeat-until-fails function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,8 +6,12 @@ alias externalip="echo -n $(curl --silent 'http://checkip.amazonaws.com')"
 
 # Run a command repeatedly until it fails
 function repeat-until-fails () {
-  exit_code=0
-  count=0
+  if [[ $# -eq 0 ]]; then
+    echo "Usage: ${0} command" >&2
+    return 1
+  fi
+  local exit_code=0
+  local count=0
   while [ $exit_code -eq 0 ]; do
     count=$((count + 1))
     $@
