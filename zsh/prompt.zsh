77ec3f26ba02a4a90a6bcd9ae7a6f83389eb8074
Nick Watts <nick@nawatts.com>
2017-03-19 12:23:16 -0400

    Remove SVN plugin

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,6 +2,7 @@ PROMPT='%{$fg[cyan]%}[%~% ]%(?.%{$fg[green]%}.%{$fg[red]%})%B$%b '
 
 ZSH_PROMPT_BASE_COLOR="%{$reset_color%}"
 
+# https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/git.zsh
 ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[green]%}"
 ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
 
@@ -14,31 +15,14 @@ ZSH_THEME_GIT_PROMPT_RENAMED="%{$fg[blue]%}▴%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_UNMERGED="%{$fg[cyan]%}§%{$reset_color%} "
 ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[white]%}◒%{$reset_color%}"
 
-ZSH_THEME_SVN_PROMPT_CLEAN=""
-ZSH_THEME_SVN_PROMPT_ADDITIONS=$ZSH_THEME_GIT_PROMPT_ADDED
-ZSH_THEME_SVN_PROMPT_DELETIONS=$ZSH_THEME_GIT_PROMPT_DELETED
-ZSH_THEME_SVN_PROMPT_MODIFICATIONS=$ZSH_THEME_GIT_PROMPT_MODIFIED
-ZSH_THEME_SVN_PROMPT_REPLACEMENTS=$ZSH_THEME_GIT_PROMPT_RENAMED
-ZSH_THEME_SVN_PROMPT_UNTRACKED=$ZSH_THEME_GIT_PROMPT_UNTRACKED
-ZSH_THEME_SVN_PROMPT_DIRTY=$ZSH_THEME_GIT_PROMPT_DIRTY
-
 ZSH_THEME_BRANCH_NAME_COLOR="%{$fg[white]%}"
-
-ZSH_THEME_SVN_PROMPT_PREFIX=""
-ZSH_THEME_SVN_PROMPT_SUFFIX=""
 
 function git_prompt() {
   echo "$(git_prompt_status)%{$reset_color%} $(git_current_branch)"
 }
 
-# The branch name function from oh-my-zsh is case sensitive.
-# This is a copy of that function with -i flag added to egrep
-function svn_current_branch_name() {
-  grep '^URL:' <<< "${1:-$(svn info 2> /dev/null)}" | egrep -io '(tags|branches)/[^/]+|trunk'
-}
-
 function rprompt_info() {
-  local info="$(git_prompt)$(svn_prompt_info)"
+  local info="$(git_prompt)"
   local venv_info=$(virtualenv_prompt_info)
   if [[ $venv_info ]]; then
     info="$info $venv_info"
